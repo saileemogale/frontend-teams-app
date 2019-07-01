6df12de64e018df56415bf0c412b9bc2229313fc
Sailee Mogale <sailee.reddy@gmail.com>
2019-07-02 02:49:15 +0530

    Corrected linting

--- src/main.js
+++ src/main.js
@@ -3,14 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//import BootstrapVue from 'bootstrap-vue'
-
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-//import VueLazyLoad from 'vue-lazyload'
-import lightbox from 'vlightbox';
-
+import lightbox from 'vlightbox'
 
 Vue.config.productionTip = false
 
@@ -19,6 +12,8 @@ Vue.use(lightbox)
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
